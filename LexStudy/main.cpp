a54dad879fb02970cde2ed3fb194da2c8ab2f980
yangxuefeng <yangxuefeng@yangxuefengdeiMac.local>
2018-03-10 00:10:57 +0800

    Initial Commit

---
+++ LexStudy/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LexStudy
+//
+//  Created by  yangxuefeng on 2018/3/10.
+//  Copyright © 2018年  yangxuefeng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
